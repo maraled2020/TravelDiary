527033a7b37285bc81f2ae46206250a3a96e9be7
maraled2020 <maraled2020@gmail.com>
2014-09-26 20:12:55 +0100

    initial commit

---
+++ app/src/main/java/com/khodadadi/delaram/traveldiary/MainActivity.java
@@ -0,0 +1,16 @@
+package com.khodadadi.delaram.traveldiary;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by deli on 9/26/2014.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
