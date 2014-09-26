82fb613250d628b59bcee7ef461f47397167eaf9
hoomi <hoomi2010@gmail.com>
2014-09-26 21:38:29 +0100

    Fixes #2

--- app/src/main/java/com/khodadadi/delaram/traveldiary/MainActivity.java
+++ app/src/main/java/com/khodadadi/delaram/traveldiary/MainActivity.java
@@ -3,9 +3,6 @@ package com.khodadadi.delaram.traveldiary;
 import android.app.Activity;
 import android.os.Bundle;
 
-/**
- * Created by deli on 9/26/2014.
- */
 public class MainActivity extends Activity {
 
     @Override
